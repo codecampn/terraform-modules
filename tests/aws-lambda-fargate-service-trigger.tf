3c798a3d5adc55da22acd8b6ca76b28187357802
Nora Schille <nora.schille@codecamp-n.com>
2020-08-25 20:16:36 +0200

    add missing region to test

--- tests/aws-lambda-fargate-service-trigger.tf
+++ tests/aws-lambda-fargate-service-trigger.tf
@@ -2,6 +2,7 @@ module "aws-lambda-fargate-service-trigger" {
   source                     = "../aws/lambda-fargate-service-trigger"
   project                    = "test"
   stage                      = "test"
+  region                     = "us-east-1"
   ecs_cluster                = "test_cluster"
   ecs_service_names          = ["test_service_name"]
   start_scheduled_expression = "cron(0 4 * * ? *)"
